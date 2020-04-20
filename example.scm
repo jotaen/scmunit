86c687aa2b44e26211d5fd8a930fa58968bd893a
Jan Heuermann <jan@jotaen.net>
2020-04-20 16:23:03 +0200

    Whitespace discipline

--- example.scm
+++ example.scm
@@ -4,11 +4,11 @@
 
 (testcase* "abs" (list
 
-	(testcase "returns non-negative numbers as is" (list
-	  (assert eq? (abs 0) 0)
+  (testcase "returns non-negative numbers as is" (list
+    (assert eq? (abs 0) 0)
     (assert eq? (abs 1) 1)
     (assert eq? (abs 184) 184)
-	))
+  ))
 
   (testcase "inverts sign of negative numbers" (list
     (assert eq? (abs -182) 182)
